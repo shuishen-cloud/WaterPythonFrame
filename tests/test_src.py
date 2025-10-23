591d2ee0e06eaca56c1b776318a66b825ecf32c1
Wsl_Arch_lwy <201212ss@sina.com>
2025-10-23 21:24:47 +0800

    test(): 测试项目包索引是否完善（完善）
    - 注释文档化测试（成功）
    
    - test(launch.json): 编写 Vscode 的调试方案

---
+++ tests/test_src.py
@@ -0,0 +1,15 @@
+'''
+Filename:  test_src.py
+Project:   tests
+Author:    lwy
+***
+Created:   2025/10/23 Thursday 20:52:27
+Modified:  2025/10/23 Thursday 20:52:31
+***
+Description: 
+'''
+
+from WaterFrameWork.core.test_core import test_core_func, test_core_func2
+
+test_core_func()
+test_core_func2()
